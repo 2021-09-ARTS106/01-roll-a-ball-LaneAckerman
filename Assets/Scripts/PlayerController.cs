9825d7c25f4c073580dd55e84795dd9cb7676d2f
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2021-09-08 11:26:44 -0400

    phase 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //declares Rigidbody variable
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
+    //provides movement data
     void onMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.Y;
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
     }
 }
